25fe9617c695bc7ae96a75415117c95d6e0c3250
Davi Simão <davbsimao@gmail.com>
2026-08-05 17:28:22 -0300

    refactor(database): expose JDBC connection factory

--- src/main/java/com/davi/library/connection/ConnectionFactory.java
+++ src/main/java/com/davi/library/connection/ConnectionFactory.java
@@ -5,17 +5,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
-    public static void main(String[] args) {
+    public Connection getConnection() throws SQLException {
+
         String url = System.getenv().getOrDefault("DB_URL", "jdbc:mysql://localhost:3307/library_db");
         String user = System.getenv().getOrDefault("DB_USER", "root");
         String password = System.getenv("DB_PASSWORD");
 
-        try {
-            Connection connection = DriverManager.getConnection(url, user, password);
-            System.out.println("Connection successfully ");
-            connection.close();
-        } catch (SQLException e) {
-            System.out.println("Connection error");
-        }
+        return DriverManager.getConnection(url, user, password);
+
+
     }
 }
